b0d7b45beedd532f7dad0ad97807a09f0350a3a8
Svetlana Govyazina <govyazina@gmail.com>
2023-04-06 12:18:14 +0300

    added check for number

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Итерационный подсчет факториала
 function factIterat(num){
     let result = 1;
+    if (num < 0 || (num % 1 !== 0)) {
+        return 'введите целое положительное число ¯\\_(ツ)_/¯ '
+    }
     while (num > 0) {
         result *= num
         num--
@@ -15,5 +18,5 @@ function factRec(num){
     return result
 }
 
-console.log('итеративный метод: ',factIterat(3))
+console.log('итеративный метод: ',factIterat(3.2))
 console.log('рекурсивный метод: ',factRec(3))
