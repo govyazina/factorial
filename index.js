661cf08171a2627016d76246ab5ee23849a84836
Svetlana Govyazina <govyazina@gmail.com>
2023-04-06 10:55:27 +0300

    console logs added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Итерационный подсчет факториала
 function factIterat(num){
+    let result = 0;
 
+    return result
 }
 
 // Рекурсивный расчет факториала
 function factRec(num){
- 
+    let result = 0;
+
+    return result
 }
+
+console.log('итеративный метод: ',factIterat(3))
+console.log('рекурсивный метод: ',factRec(3))
